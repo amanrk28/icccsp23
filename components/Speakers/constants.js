4e69808acf4d9032d7dafd230702db1d6f534451
AmanRK <amankhemka.ak28@gmail.com>
2022-05-01 11:34:35 +0530

    Speakers list updated

---
+++ components/Speakers/constants.js
@@ -0,0 +1,32 @@
+export const SPEAKERS = [
+  {
+    image: 'https://icccsp.com/img/Xiaoli_Li.jpeg',
+    name: 'Dr. Xiaoli Li',
+    designation:
+      'Department Head (Machine Intellection department) Institute for Infocomm Research, Adjunct Professor, School of Computer Science and Engineering',
+    description: 'Nanyang Technological University, Singapore',
+    profile: 'https://personal.ntu.edu.sg/xlli/',
+  },
+  {
+    image: 'https://icccsp.com/img/Ponnuthurai.png',
+    name: 'Dr. Ponnuthurai Nagaratnam Suganthan',
+    designation: 'Professor, School for Electrical and Electronic Engineering',
+    description: 'Nanyang Technological University, Singapore',
+    profile: 'https://www3.ntu.edu.sg/home/epnsugan/',
+  },
+  {
+    image: 'https://icccsp.com/img/Subhashini.png',
+    name: 'Dr. Subhashini Venugopalan',
+    designation: 'Research Scientist',
+    description: 'Google Research, California',
+    profile: 'https://vsubhashini.github.io/',
+  },
+  {
+    image: 'https://icccsp.com/img/kun_chan.png',
+    name: 'Dr. Kun-Chan Lan',
+    designation:
+      'Professor, Dept. of Computer Science and Information Engineering',
+    description: 'National Cheng Kung University Taiwan',
+    profile: 'https://www.csie.ncku.edu.tw/~klan/',
+  },
+];
--- components/Speakers/index.js
+++ components/Speakers/index.js
@@ -1,3 +1,5 @@
+import { SPEAKERS } from './constants';
+
 export default function Speakers() {
   return (
     <div className="speakersContainer" id="speakers">
@@ -5,7 +7,26 @@ export default function Speakers() {
         <b>SPEAKERS</b>
       </h1>
       <br />
-      <h4>Will be Updated Soon...</h4>
+      <div className="section-wrapper">
+        <div className="row cards-wrapper">
+          {SPEAKERS.map(item => (
+            <a
+              key={item.name}
+              href={item.profile}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="card"
+            >
+              <img src={item.image} alt="Card image cap" />
+              <div className="card-body">
+                <h1 className="card-title">{item.name}</h1>
+                <p className="card-text">{item.designation}</p>
+                <p className="card-text">{item.description}</p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
