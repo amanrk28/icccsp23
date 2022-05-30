898ec49a28902424256d689103a976915cc2ae08
AmanRK <amankhemka.ak28@gmail.com>
2022-05-30 22:13:19 +0530

    authors section added

--- components/Authors/index.js
+++ components/Authors/index.js
@@ -5,7 +5,60 @@ export default function Authors() {
         <b>AUTHORS</b>
       </h1>
       <br />
-      <h4>Will be Updated Soon...</h4>
+      <h3>Paper Submission & Guidelines</h3>
+      <p>
+        Authors can contribute regular paper with scientific / technical
+        research works, survey works and industrial experiences that include
+        significant advances in field of Artificial Intelligence, Knowledge
+        Engineering and IoT.  Authors should write their papers in British
+        English. The paper should not exceed 15 pages including references.
+        <br /> The submission should be novel, plagiarism free and not
+        simultaneously submitted to another conference. Submissions must include
+        title, abstract, list of keywords, introduction, literature review,
+        methodology, results / discussion, and conclusion. All submissions will
+        be subjected to a double‐blind review process by at least three
+        reviewers. Authors should abide Springer’s authors’ guidelines and use
+        their proceedings templates, either LaTeX or Word, for the preparation
+        of their papers.
+      </p>
+      <h3>Templates</h3>
+      <a href="https://icccsp.com/WordTemplates.zip" download>
+        Click here For Sample Springer Paper Format in Word
+      </a>
+      <br />
+      <a href="https://icccsp.com/LaTexTemplates.zip" download>
+        Click here For Sample Springer Paper Format (LaTex Template)
+      </a>
+      <p>
+        For more detailed guidelines, please visit{' '}
+        <span>
+          <a
+            href="http://preview.springer.com/gp/computer-science/lncs/conference-proceedings-guidelines"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            official Springer Link
+          </a>
+        </span>
+        <br />
+      </p>
+      <h3>Submission</h3>
+      <p>
+        The manuscript should be submitted in PDF format using the conference
+        management system Easychair, in the following link: <br />
+        <a
+          href="https://easychair.org/conferences/?conf=icccsp2023"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          https://easychair.org/conferences/?conf=icccsp2023
+        </a>
+      </p>
+      <h3>Registration</h3>
+      <p>
+        At least one author of each accepted paper must register for the
+        conference indicated on the conference website and present the paper.
+      </p>
     </div>
   );
 }
